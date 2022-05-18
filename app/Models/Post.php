6284908a8d12186dfd19061b9580712334b9caab
unknown <spiderofegypt98@gmail.com>
2022-05-18 15:15:12 +0200

    laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model 
+{
+
+    protected $table = 'posts';
+    public $timestamps = true;
+    protected $fillable = array('name', 'catetgory_id','user_id');
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag', 'post_tag', 'tag_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+}
